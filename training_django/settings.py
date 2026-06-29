b08ee40953e4586cbe63e3801f02be2e993a29d0
faysal077 <shadmanfaysal053@gmail.com>
2026-06-29 13:30:08 +0600

    Merged with Dev-Ops Project

--- training_django/settings.py
+++ training_django/settings.py
@@ -73,7 +73,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
 
 INSTALLED_APPS = [
     'django_prometheus',
-    'django_prometheus',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
